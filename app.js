e38b8d07346b93a45956ae2dfac4c2582998fc78
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2022-12-22 04:44:40 +0530

    Updated to the template file provided by PF

--- app.js
+++ app.js
@@ -1,40 +1,62 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-
-// Models
 const { Todo } = require("./models");
-
-// Middleware
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-// Routes
-// app.get('/todos', (req, res) => {});
+app.get("/", function (request, response) {
+  response.send("Hello World");
+});
 
-app.post("/todos", async (req, res) => {
+// eslint-disable-next-line no-unused-vars
+app.get("/todos", async function (request, response) {
+  console.log("Processing list of all Todos ...");
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
+  // Then, we have to respond with all Todos, like:
+  // response.send(todos)
+});
+
+app.get("/todos/:id", async function (request, response) {
   try {
-    const todo = await Todo.addTodo({
-      title: req.body.title,
-      dueDate: req.body.dueDate,
-    });
-    return res.json(todo);
+    const todo = await Todo.findByPk(request.params.id);
+    return response.json(todo);
   } catch (error) {
     console.log(error);
-    return res.status(422).json(error);
+    return response.status(422).json(error);
   }
 });
 
-app.put("/todos/:id/markAsCompleted", async (req, res) => {
-  const todo = await Todo.findByPk(req.params.id);
+app.post("/todos", async function (request, response) {
+  try {
+    const todo = await Todo.addTodo(request.body);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async function (request, response) {
+  const todo = await Todo.findByPk(request.params.id);
   try {
     const updatedTodo = await todo.markAsCompleted();
-    return res.json(updatedTodo);
+    return response.json(updatedTodo);
   } catch (error) {
     console.log(error);
-    return res.status(422).json(error);
+    return response.status(422).json(error);
   }
 });
 
-// app.delete('/todos/:id', (req, res) => {});
+// eslint-disable-next-line no-unused-vars
+app.delete("/todos/:id", async function (request, response) {
+  console.log("We have to delete a Todo with ID: ", request.params.id);
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our database to delete a Todo by ID.
+  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
+  // response.send(true)
+});
 
 module.exports = app;
