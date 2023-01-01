5a61c77ca87cc1c0f5ad06b52939c30423d24154
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-01-01 13:53:58 +0530

    Created association between User and Todo tables

--- models/user.js
+++ models/user.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Todo, {
+        foreignKey: "userId",
+      });
     }
   }
   User.init(
