0ad9873fafd532a8b23054471c6bd553fe7068ab
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2024-04-24 23:06:12 +0530

    Update database configuration in config.js

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: "postgres",
+    password: "postgres",
+    database: "wd-todo-dev",
     host: "127.0.0.1",
     port: 5433,
     dialect: "postgres",
